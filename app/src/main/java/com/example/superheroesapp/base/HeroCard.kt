ed6d6e603693490dfe77788bd813138a178c3b3d
AlexBunt <Buntov2003@mail.ru>
2025-07-23 23:52:16 +0300

    OOP principal

--- app/src/main/java/com/example/superheroesapp/base/HeroCard.kt
+++ app/src/main/java/com/example/superheroesapp/base/HeroCard.kt
@@ -11,22 +11,93 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroesapp.R
+import com.example.superheroesapp.model.Hero
 import com.example.superheroesapp.ui.theme.SuperheroesAppTheme
 
 @Composable
 internal fun HeroCard (
+    hero: Hero,
+    modifier: Modifier = Modifier
+){
+    Card (
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            contentColor = MaterialTheme.colorScheme.tertiaryContainer
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp
+        )
+    ) {
+        Row (
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(72.dp)
+        ) {
+            HeroNameAndInfo(
+                hero.nameResId,
+                hero.descriptionId,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            HeroIcon(hero.imageResId,)
+        }
+    }
+}
+
+@Composable
+private fun HeroIcon(
+    heroIcon: Int,
+    modifier: Modifier = Modifier
+){
+    Image(
+        modifier = modifier
+            .size(72.dp)
+            .clip(MaterialTheme.shapes.small),
+        painter = painterResource(heroIcon),
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+private fun HeroNameAndInfo(
+    heroName: Int,
+    heroInfo: Int,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(heroName),
+            style = MaterialTheme.typography.displaySmall,
+        )
+        Text(
+            text = stringResource(heroInfo),
+            style = MaterialTheme.typography.bodyMedium,
+
+            )
+    }
+}
+
+
+@Composable
+internal fun HeroCardForTest (
     modifier: Modifier = Modifier
 ){
     Card (
@@ -47,13 +118,13 @@ internal fun HeroCard (
                     text = stringResource(R.string.description1),
                     style = MaterialTheme.typography.bodyMedium,
 
-                )
+                    )
             }
             Spacer(modifier = Modifier.width(16.dp))
             Image(
                 modifier = Modifier
-                .size(72.dp)
-                .clip(MaterialTheme.shapes.small),
+                    .size(72.dp)
+                    .clip(MaterialTheme.shapes.small),
                 painter = painterResource(R.drawable.android_superhero1),
                 contentDescription = null,
                 contentScale = ContentScale.Crop
@@ -67,6 +138,6 @@ internal fun HeroCard (
 @Composable
 private fun CardPreview(){
     SuperheroesAppTheme {
-        HeroCard()
+        HeroCardForTest()
     }
 }
