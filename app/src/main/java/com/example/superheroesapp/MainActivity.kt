a81c952378c9c1332ed3445caef2f9903b88dc34
AlexBunt <Buntov2003@mail.ru>
2025-07-26 22:28:15 +0300

    Welcome animation

--- app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,11 +20,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroesapp.base.HeroItem
-import com.example.superheroesapp.model.Heroes
 import com.example.superheroesapp.model.Heroes.heroes
 import com.example.superheroesapp.ui.theme.SuperheroesAppTheme
 
@@ -57,15 +62,29 @@ class MainActivity : ComponentActivity() {
 private fun SuperheroesApp (
     modifier: Modifier = Modifier
 ){
-    LazyColumn(modifier){
-        items (items = heroes) {
-            HeroItem(
-                hero = it,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(bottom = 8.dp)
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            targetState = true
+        }
+    }
+
+    AnimatedVisibility(
+        visibleState = visibleState,
+        enter = fadeIn(
+            animationSpec = tween(1000)
+        ),
+        exit = fadeOut()
+    ) {
+        LazyColumn(modifier) {
+            items(items = heroes) {
+                HeroItem(
+                    hero = it,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .padding(bottom = 8.dp)
                 )
+            }
         }
     }
 }
